38f6be8d48a89ce0ec038adbcd009034c495e36e
Kevin Driscoll <kpdriscoll6@gmail.com>
2024-09-02 16:39:49 -0600

    update

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 from typing import Optional
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # React's default port
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/items")
+async def get_items():
+    return {"items": ["Item 1", "Item 2", "Item 3"]}
