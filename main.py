0613301264f80ed1de9dfd4f56e3708086e40199
Kevin Driscoll <kpdriscoll6@gmail.com>
2024-09-03 10:22:41 -0600

    update

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5000","https://*.onrender.com"],
-    #allow_origins=["https://react-fastapi-boilerplate-tygo.onrender.com", "http://localhost:3000"],
+    #allow_origins=["http://localhost:3000", "http://localhost:5000","https://*.onrender.com"],
+    allow_origins=["https://react-fastapi-boilerplate-tygo.onrender.com", "http://localhost:3000"],
     #allow_origins=["*"],  # Allows all origins
     allow_credentials=True,
     allow_methods=["*"],
