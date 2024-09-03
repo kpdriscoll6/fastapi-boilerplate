b68eb04ef39045909eaf0294e6bab99950c52ad4
Kevin Driscoll <kpdriscoll6@gmail.com>
2024-09-02 18:58:52 -0600

    update_for deployment

--- main.py
+++ main.py
@@ -2,12 +2,19 @@ from typing import Optional
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+ENVIRONMENT = os.getenv("ENVIRONMENT", "development")
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # React's default port
+    allow_origins=["http://localhost:3000", "http://localhost:5000","https://*.onrender.com"],
+    #allow_origins=["*"],  # Allows all origins
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
